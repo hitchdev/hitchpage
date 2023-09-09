6d60d6dafb75cf6c57ddd6ec07d528d2146fe5dc
Colm O'Connor <colm.oconnor.github@gmail.com>
2023-09-09 11:54:33 +0100

    REFACTOR : Prepare for the use of an ElementLookup class.

--- hitchpage/model.py
+++ hitchpage/model.py
@@ -3,32 +3,50 @@ from pathlib import Path
 from strictyaml import load, MapPattern, Optional, Str, Map
 
 
+
+class ElementLookup:
+    def __init__(self, conf_dict):
+        self._conf_dict = conf_dict
+
+    def __getitem__(self, key):
+        return self._conf_dict[key]
+
+
 class PlaywrightPageConfig:
     def __init__(self, *config_files: Path, playwright_page: PlaywrightPage):
         self._config_files = config_files
         self._playwright_page = playwright_page
         self._current_page = None
-        self._config_dict = load(
-            self._config_files[0].read_text(),
-            MapPattern(
-                Str(),
-                Map(
-                    {
-                        "element": MapPattern(
-                            Str(),
-                            Str()
-                            | Map(
-                                {
-                                    Optional("text"): Str(),
-                                    Optional("locator"): Str(),
-                                    Optional("in iframe"): Str(),
-                                }
-                            ),
-                        )
-                    }
+        self._config_dict = {
+            page_name: {
+                "element": {
+                    element: element_conf
+                    for element, element_conf in 
+                    page_conf["element"].items()
+                }
+            } for page_name, page_conf in 
+            load(
+                self._config_files[0].read_text(),
+                MapPattern(
+                    Str(),
+                    Map(
+                        {
+                            "element": MapPattern(
+                                Str(),
+                                Str()
+                                | Map(
+                                    {
+                                        Optional("text"): Str(),
+                                        Optional("locator"): Str(),
+                                        Optional("in iframe"): Str(),
+                                    }
+                                ),
+                            )
+                        }
+                    ),
                 ),
-            ),
-        ).data
+            ).data.items()
+        }
 
     def next_page(self, new_page: str):
         self._current_page = new_page
@@ -39,8 +57,20 @@ class PlaywrightPageConfig:
 
     def _get_iframe(self, which_iframe):
         return self._playwright_page.frame_locator(
-            self._page_conf["element"][which_iframe]["locator"]
+            self._locator(which_iframe)
         )
+    
+    def _locator(self, name):
+        element_conf = self._page_conf["element"][name]
+        if isinstance(element_conf, str):
+            return element_conf
+        elif isinstance(element_conf, dict):
+            if "locator" in element_conf:
+                return element_conf["locator"]
+            else:
+                raise Exception(f"Must use locator for {name} in {self._current_page}")
+        else:
+            raise Exception("Bad error")
 
     def element(self, name):
         element_dict = self._page_conf["element"][name]
@@ -57,4 +87,4 @@ class PlaywrightPageConfig:
             elif "text" in element_dict:
                 return page.get_by_text(element_dict["text"])
             else:
-                raise Exception("Unknown")
+                raise Exception("Bad error")
